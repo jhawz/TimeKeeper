9cee6b0cb1e1d9ce961f5382094330a1a7f9eb0a
jhawz <jhause@gmail.com>
2016-03-16 14:40:51 -0400

    Active directory authentication works. New window pops when authorized, error toast pops when wrong un pw or domain.

--- LoginWindow.xaml.cs
+++ LoginWindow.xaml.cs
@@ -11,22 +11,60 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.DirectoryServices;
 
 namespace TimeKeeper
 {
     /// <summary>
     /// Interaction logic for Window1.xaml
     /// </summary>
-    public partial class Window1 : Window
+    public partial class LoginWindow : Window
     {
-        public Window1()
+        public static Credentials credentials;
+        private LdapAuthentication ldapAuthentication;
+
+        public LoginWindow()
         {
             InitializeComponent();
+            credentials = new Credentials();
+            string adPath = DomainManager.RootPath;
+            ldapAuthentication = new LdapAuthentication(adPath);
         }
 
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        private void TextBox_UserName(object sender, TextChangedEventArgs e)
         {
+            credentials.UserName = UserNameTextBox.Text;
+        }
 
+        private void TextBox_Domain(object sender, TextChangedEventArgs e)
+        {
+            credentials.Domain = DomainTextBox.Text;
+        }
+
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            credentials.Password = PasswordBox.Password;
+        }
+
+        private void Login_Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                credentials.Authenticated = ldapAuthentication.IsAuthenticated(credentials.Domain, credentials.UserName, credentials.Password);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (credentials.Authenticated.Equals(true))
+                {
+                    MainWindow mainWindow = new MainWindow();
+                    mainWindow.Show();
+                    this.Close();
+                }
+            }
         }
     }
 }
